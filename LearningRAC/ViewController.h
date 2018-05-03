b7c65a43db62d6204da7aceac8f72cfc2d8ac17e
zhurui <www.515246872@qq.com>
2018-05-03 10:17:24 +0800

    first commit

---
+++ LearningRAC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LearningRAC
+//
+//  Created by 朱锐 on 2018/5/3.
+//  Copyright © 2018年 朱锐. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LearningRAC/ViewController.m
@@ -0,0 +1,48 @@
+//
+//  ViewController.m
+//  LearningRAC
+//
+//  Created by 朱锐 on 2018/5/3.
+//  Copyright © 2018年 朱锐. All rights reserved.
+//
+
+#import "ViewController.h"
+
+#import <ReactiveObjC/ReactiveObjC.h>
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    RACSignal * signal = [RACSignal createSignal:^RACDisposable * _Nullable(id<RACSubscriber>  _Nonnull subscriber) {
+        
+        [subscriber sendNext:@"message"];
+        
+        [subscriber sendCompleted];
+        
+        return [RACDisposable disposableWithBlock:^{
+            NSLog(@"信号销毁");
+        }];
+        
+    }];
+    
+    [signal subscribeNext:^(id  _Nullable x) {
+        NSLog(@"接收到数据--%@",x);
+    }];
+    
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
